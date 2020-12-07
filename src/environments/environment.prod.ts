cd9cda1a2e0a9c71d18c297ea929a041125306f5
Yehuda Buzaglo <yehudab@codevalue.com>
2020-12-07 17:30:40 +0200

    push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCC_27XFGgDxCTnf3NqfmVeq07FKUsCZm0',
+    authDomain: 'homez-162d4.firebaseapp.com',
+    projectId: 'homez-162d4',
+    storageBucket: 'homez-162d4.appspot.com',
+    messagingSenderId: '322288373004',
+    appId: '1:322288373004:web:7c512be805425858944d29',
+    measurementId: 'G-QC4LJDD7DV',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCC_27XFGgDxCTnf3NqfmVeq07FKUsCZm0",
+    authDomain: "homez-162d4.firebaseapp.com",
+    projectId: "homez-162d4",
+    storageBucket: "homez-162d4.appspot.com",
+    messagingSenderId: "322288373004",
+    appId: "1:322288373004:web:7c512be805425858944d29",
+    measurementId: "G-QC4LJDD7DV"
+  }
 };
 
 /*
